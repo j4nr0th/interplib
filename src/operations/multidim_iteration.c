db5ca94fee4c72d3b09b8bc77d9af55c574fb9a0
jan <r0th.j4n@gmail.com>
2025-11-22 20:54:03 +0100

    renamed nd-iterator to multidim_iterator

---
+++ src/operations/multidim_iteration.c
@@ -0,0 +1,175 @@
+
+#include "multidim_iteration.h"
+
+static inline size_t *multidim_iterator_dims_ptr(multidim_iterator_t *this)
+{
+    return this->dims_and_offsets;
+}
+
+static inline size_t *multidim_iterator_offsets_ptr(multidim_iterator_t *this)
+{
+    return this->dims_and_offsets + this->ndims;
+}
+
+static inline const size_t *multidim_iterator_dims_const_ptr(const multidim_iterator_t *this)
+{
+    return this->dims_and_offsets;
+}
+
+static inline const size_t *multidim_iterator_offsets_const_ptr(const multidim_iterator_t *this)
+{
+    return this->dims_and_offsets + this->ndims;
+}
+
+size_t multidim_iterator_needed_memory(const size_t ndims)
+{
+    return sizeof(multidim_iterator_t) + ndims * sizeof(*((multidim_iterator_t *)0xB00B5)->dims_and_offsets);
+}
+
+void multidim_iterator_init(multidim_iterator_t *const this, const size_t ndims,
+                            const size_t INTERPLIB_ARRAY_ARG(dims, const static ndims))
+{
+    this->ndims = ndims;
+    size_t *const iter_dims = multidim_iterator_dims_ptr(this);
+    size_t *const iter_offsets = multidim_iterator_offsets_ptr(this);
+    for (size_t i = 0; i < ndims; ++i)
+    {
+        iter_dims[i] = dims[i];
+        iter_offsets[i] = 0;
+    }
+}
+void multidim_iterator_set_to_start(multidim_iterator_t *this)
+{
+    // Reset offsets
+    size_t *const iter_offsets = multidim_iterator_offsets_ptr(this);
+    for (size_t i = 0; i < this->ndims; ++i)
+    {
+        iter_offsets[i] = 0;
+    }
+}
+
+void multidim_iterator_set_to_end(multidim_iterator_t *this)
+{
+    size_t *const iter_offsets = multidim_iterator_offsets_ptr(this);
+    const size_t *const iter_dims = multidim_iterator_dims_const_ptr(this);
+    for (size_t i = 0; i < this->ndims - 1; ++i)
+    {
+        iter_offsets[i] = iter_dims[i] - 1;
+    }
+    iter_offsets[this->ndims - 1] = iter_dims[this->ndims - 1];
+}
+
+void multidim_iterator_advance(multidim_iterator_t *const this, size_t dim, size_t step)
+{
+    ASSERT(dim < this->ndims, "Dimension out of bounds");
+    const size_t *const iter_dims = multidim_iterator_dims_const_ptr(this);
+    size_t *const iter_offsets = multidim_iterator_offsets_ptr(this);
+
+compute_offset:;
+    size_t new_offset = iter_offsets[dim] + step;
+    if (new_offset < iter_dims[dim])
+    {
+        // the step is fine
+        iter_offsets[dim] = new_offset;
+        return;
+    }
+
+    // We would step out of bounds
+    if (dim == 0)
+    {
+        multidim_iterator_set_to_end(this);
+        return;
+    }
+
+    // We should step the previous dimension as well
+
+    const size_t stride_lower_dim = new_offset / iter_dims[dim];
+    new_offset %= iter_dims[dim];
+    iter_offsets[dim] = new_offset;
+
+    // Tail recursion (but with goto)
+    // multidim_iterator_advance(this, dim - 1, stride_lower_dim);
+    dim = dim - 1;
+    step = stride_lower_dim;
+
+    goto compute_offset;
+}
+void multidim_iterator_recede(multidim_iterator_t *this, size_t dim, size_t step)
+{
+    ASSERT(dim < this->ndims, "Dimension out of bounds");
+    const size_t *const iter_dims = multidim_iterator_dims_const_ptr(this);
+    size_t *const iter_offsets = multidim_iterator_offsets_ptr(this);
+
+compute_offset:;
+    if (step <= iter_offsets[dim])
+    {
+        // the step is fine
+        iter_offsets[dim] -= step;
+        return;
+    }
+    // We would step out of bounds
+    if (dim == 0)
+    {
+        multidim_iterator_set_to_start(this);
+        return;
+    }
+
+    const size_t remaining = step - iter_offsets[dim];
+    // Ceil-divide
+    const size_t required_steps = (remaining + iter_dims[dim] - 1) / iter_dims[dim];
+
+    iter_offsets[dim] = iter_offsets[dim] + required_steps * iter_dims[dim] - step;
+    // Tail recursion (but with goto)
+    // multidim_iterator_recede(this, dim - 1, required_steps);
+    dim -= 1;
+    step = required_steps;
+
+    goto compute_offset;
+}
+int multidim_iterator_is_at_start(const multidim_iterator_t *this)
+{
+    const size_t *const iter_offsets = multidim_iterator_offsets_const_ptr(this);
+    for (unsigned i = 0; i < this->ndims; ++i)
+    {
+        if (iter_offsets[i] != 0)
+            return 0;
+    }
+    return 1;
+}
+
+int multidim_iterator_is_at_end(const multidim_iterator_t *this)
+{
+    const size_t *const iter_offsets = multidim_iterator_offsets_const_ptr(this);
+    const size_t *const iter_dims = multidim_iterator_dims_const_ptr(this);
+    for (unsigned i = 0; i < this->ndims - 1; ++i)
+    {
+        if (iter_offsets[i] != iter_dims[i] - 1)
+            return 0;
+    }
+    return iter_offsets[this->ndims - 1] == iter_dims[this->ndims - 1];
+}
+size_t multidim_iterator_get_flat_index(const multidim_iterator_t *this)
+{
+    size_t index = 0;
+    const size_t *const iter_offsets = multidim_iterator_offsets_const_ptr(this);
+    const size_t *const iter_dims = multidim_iterator_dims_const_ptr(this);
+    for (unsigned i = 0; i < this->ndims; ++i)
+    {
+        index *= iter_dims[i];
+        index += iter_offsets[i];
+    }
+    return index;
+}
+size_t multidim_iterator_get_ndims(const multidim_iterator_t *this)
+{
+    return this->ndims;
+}
+
+const size_t *multidim_iterator_dims(const multidim_iterator_t *this)
+{
+    return multidim_iterator_dims_const_ptr(this);
+}
+const size_t *multidim_iterator_offsets(const multidim_iterator_t *this)
+{
+    return multidim_iterator_offsets_const_ptr(this);
+}
--- src/operations/multidim_iteration.h
+++ src/operations/multidim_iteration.h
@@ -8,37 +8,38 @@ typedef struct
 {
     size_t ndims;
     size_t dims_and_offsets[];
-} nd_iterator_t;
+} multidim_iterator_t;
 
 /**
- * Calculates the amount of memory needed to store a nd_iterator_t structure
+ * Calculates the amount of memory needed to store a multidim_iterator_t structure
  * with the specified number of dimensions.
  *
  * @param ndims The number of dimensions for the iterator.
- * @return The size in bytes of memory required to initialize the nd_iterator_t
+ * @return The size in bytes of memory required to initialize the multidim_iterator_t
  *         structure with the given number of dimensions.
  */
-size_t nd_iterator_needed_memory(size_t ndims);
+size_t multidim_iterator_needed_memory(size_t ndims);
 
 /**
- * Initializes a nd_iterator_t structure with the specified number of dimensions
+ * Initializes a multidim_iterator_t structure with the specified number of dimensions
  * and dimension sizes. This function sets up the necessary internal data for
  * an iterator to traverse multidimensional data structures.
  *
- * @param this Pointer to the nd_iterator_t instance to be initialized.
+ * @param this Pointer to the multidim_iterator_t instance to be initialized.
  * @param ndims The number of dimensions of the iterator.
  * @param dims An array of size `ndims` specifying the size of each dimension.
  */
-void nd_iterator_init(nd_iterator_t *this, size_t ndims, const size_t INTERPLIB_ARRAY_ARG(dims, static ndims));
+void multidim_iterator_init(multidim_iterator_t *this, size_t ndims,
+                            const size_t INTERPLIB_ARRAY_ARG(dims, static ndims));
 
 /**
- * Resets the offsets of the n-dimensional iterator to their starting values.
+ * Resets the offsets of the multidimensional iterator to their starting values.
  * This function repositions the iterator to the beginning of the multidimensional space.
  *
- * @param this A pointer to the nd_iterator_t structure to be reset.
+ * @param this A pointer to the multidim_iterator_t structure to be reset.
  *             The structure must be initialized before calling this function.
  */
-void nd_iterator_set_to_start(nd_iterator_t *this);
+void multidim_iterator_set_to_start(multidim_iterator_t *this);
 
 /**
  * Sets the iterator to its end position based on its dimensions.
@@ -51,7 +52,7 @@ void nd_iterator_set_to_start(nd_iterator_t *this);
  * @param this A pointer to the iterator object whose position is to be set
  *             to the end.
  */
-void nd_iterator_set_to_end(nd_iterator_t *this);
+void multidim_iterator_set_to_end(multidim_iterator_t *this);
 
 /**
  * Advances the position of the multidimensional iterator by a specified step
@@ -62,7 +63,7 @@ void nd_iterator_set_to_end(nd_iterator_t *this);
  * @param dim The dimension in which the iterator is to be advanced.
  * @param step The number of steps to advance the iterator in the specified dimension.
  */
-void nd_iterator_advance(nd_iterator_t *this, size_t dim, size_t step);
+void multidim_iterator_advance(multidim_iterator_t *this, size_t dim, size_t step);
 
 /**
  * Moves the iterator backward by a specified step in the given dimension.
@@ -71,65 +72,65 @@ void nd_iterator_advance(nd_iterator_t *this, size_t dim, size_t step);
  * the iterator will wrap around in higher dimensions as necessary. If a wrap
  * exceeds the limits of the iterator, all offsets are reset to the start.
  *
- * @param this Pointer to the nd_iterator_t structure to be modified.
+ * @param this Pointer to the multidim_iterator_t structure to be modified.
  * @param dim The dimension in which to apply the backward step. Must be
  *            less than the number of dimensions in the iterator.
  * @param step The number of steps to move backward in the specified dimension.
  */
-void nd_iterator_recede(nd_iterator_t *this, size_t dim, size_t step);
+void multidim_iterator_recede(multidim_iterator_t *this, size_t dim, size_t step);
 
 /**
- * Checks if the nd_iterator_t structure is at its starting position.
+ * Checks if the multidim_iterator_t structure is at its starting position.
  *
- * @param this Pointer to the nd_iterator_t structure to check.
+ * @param this Pointer to the multidim_iterator_t structure to check.
  * @return 1 if the iterator is at its starting position, otherwise 0.
  */
-int nd_iterator_is_at_start(const nd_iterator_t *this);
+int multidim_iterator_is_at_start(const multidim_iterator_t *this);
 
 /**
  * Checks whether the iterator has reached the end of the multidimensional space
  * it is iterating over.
  *
- * @param this A pointer to the nd_iterator_t structure representing the iterator.
+ * @param this A pointer to the multidim_iterator_t structure representing the iterator.
  * @return 1 if the iterator is at the end of its iteration space; otherwise, 0.
  */
-int nd_iterator_is_at_end(const nd_iterator_t *this);
+int multidim_iterator_is_at_end(const multidim_iterator_t *this);
 
 /**
  * Computes the flat index of the current position of the iterator in a
  * multidimensional iteration space.
  *
- * @param this A pointer to the nd_iterator_t structure representing the
+ * @param this A pointer to the multidim_iterator_t structure representing the
  *             iterator.
  * @return The flat index corresponding to the current position of the iterator.
  */
-size_t nd_iterator_get_flat_index(const nd_iterator_t *this);
+size_t multidim_iterator_get_flat_index(const multidim_iterator_t *this);
 
 /**
- * Gets the number of dimensions managed by the nd_iterator_t structure.
+ * Gets the number of dimensions managed by the multidim_iterator_t structure.
  *
- * @param this A pointer to the nd_iterator_t structure whose number of
+ * @param this A pointer to the multidim_iterator_t structure whose number of
  *             dimensions is to be retrieved.
- * @return The number of dimensions associated with the given nd_iterator_t
+ * @return The number of dimensions associated with the given multidim_iterator_t
  *         structure.
  */
-size_t nd_iterator_get_ndims(const nd_iterator_t *this);
+size_t multidim_iterator_get_ndims(const multidim_iterator_t *this);
 
 /**
  * Retrieves a pointer to the dimension array of the specified ND iterator.
  *
- * @param this A pointer to the `nd_iterator_t` structure.
+ * @param this A pointer to the `multidim_iterator_t` structure.
  * @return A pointer to the array of dimensions and offsets stored within the
  *         specified ND iterator.
  */
-const size_t *nd_iterator_dims(const nd_iterator_t *this);
+const size_t *multidim_iterator_dims(const multidim_iterator_t *this);
 
 /**
- * Retrieves a pointer to the offsets of a nd_iterator_t structure.
+ * Retrieves a pointer to the offsets of a multidim_iterator_t structure.
  *
- * @param this A pointer to the nd_iterator_t structure.
+ * @param this A pointer to the multidim_iterator_t structure.
  * @return A pointer to the array of offsets corresponding to the iterator's dimensions.
  */
-const size_t *nd_iterator_offsets(const nd_iterator_t *this);
+const size_t *multidim_iterator_offsets(const multidim_iterator_t *this);
 
 #endif // INTERPLIB_ND_ITERATION_H
