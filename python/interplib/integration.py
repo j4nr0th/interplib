6ab189205ba8245185beb22b91412caef8e130f9
jan <r0th.j4n@gmail.com>
2025-12-08 15:22:44 +0100

    feat: implement L2 projection functionality and corresponding tests

--- python/interplib/integration.py
+++ python/interplib/integration.py
@@ -6,10 +6,15 @@ import numpy as np
 import numpy.typing as npt
 
 from interplib._interp import (
+    DEFAULT_BASIS_REGISTRY,
     DEFAULT_INTEGRATION_REGISTRY,
+    BasisRegistry,
+    DegreesOfFreedom,
+    FunctionSpace,
     IntegrationRegistry,
     IntegrationSpace,
     SpaceMap,
+    compute_mass_matrix,
 )
 
 
@@ -36,34 +41,10 @@ class Integrable(Protocol):
         ...
 
 
-def integrate_callable(
-    func: Integrable,
-    integration: IntegrationSpace | SpaceMap,
-    /,
-    registry: IntegrationRegistry = DEFAULT_INTEGRATION_REGISTRY,
-) -> float:
-    """Integrate a callable over a specified integration space with given specs.
-
-    Parameters
-    ----------
-    func : Callable
-        The function to integrate. The function should be defined on the space it will
-        be integrated on.
-
-    integration_space : IntegrationSpace or SpaceMap
-        The space over which to integrate the function or the mapping between the
-        integration domain, which is an :math:`N`-dimensional :math:`[-1, +1]` hypercube,
-        and the physical domain.
-
-    registry : IntegrationRegistry, optional
-        The registry to use for obtaining the integrator. If None, the default registry is
-        used.
-
-    Returns
-    -------
-    float
-        The result of the integration.
-    """
+def _prepare_integration(
+    integration: IntegrationSpace | SpaceMap, registry: IntegrationRegistry
+) -> tuple[npt.NDArray[np.double], npt.NDArray[np.double]]:
+    """Prepare nodes and weights."""
     match integration:
         case IntegrationSpace() as int_space:
             nodes = int_space.nodes(registry)
@@ -83,8 +64,111 @@ def integrate_callable(
                 f"{type(integration)} was passed."
             )
 
+    return nodes, weights
+
+
+def integrate_callable(
+    func: Integrable,
+    integration: IntegrationSpace | SpaceMap,
+    /,
+    *,
+    registry: IntegrationRegistry = DEFAULT_INTEGRATION_REGISTRY,
+) -> float:
+    """Integrate a callable over a specified integration space with given specs.
+
+    Parameters
+    ----------
+    func : Callable
+        The function to integrate. The function should be defined on the space it will
+        be integrated on.
+
+    integration_space : IntegrationSpace or SpaceMap
+        The space over which to integrate the function or the mapping between the
+        integration domain, which is an :math:`N`-dimensional :math:`[-1, +1]` hypercube,
+        and the physical domain.
+
+    registry : IntegrationRegistry, default: DEFAULT_INTEGRATION_REGISTRY
+        The registry to use for obtaining the integrator.
+
+    Returns
+    -------
+    float
+        The result of the integration.
+    """
+    nodes, weights = _prepare_integration(integration=integration, registry=registry)
     return float(
         np.sum(
             np.asarray(func(*[nodes[i, ...] for i in range(nodes.shape[0])])) * weights
         )
     )
+
+
+def projection_l2(
+    func: Integrable,
+    function_space: FunctionSpace,
+    integration: IntegrationSpace | SpaceMap,
+    /,
+    *,
+    integration_registry: IntegrationRegistry = DEFAULT_INTEGRATION_REGISTRY,
+    basis_registry: BasisRegistry = DEFAULT_BASIS_REGISTRY,
+) -> DegreesOfFreedom:
+    """Compute the L2 projection of the function on the function space.
+
+    Parameters
+    ----------
+    func : Integratable
+        Function to project. It has to be possible to integrate it.
+
+    function_space : FunctionSpace
+        Function space on which to project the function.
+
+    integration : IntegrationSpace or SpaceMap
+        Specification of the integration domain.
+
+    integration_registry : IntegrationRegistry, default: DEFAULT_INTEGRATION_REGISTRY
+        The registry to use for obtaining the integrator.
+
+    basis_registry : BasisRegistry, default: DEFAULT_BASIS_REGISTRY
+        The registry to use for obtaining the basis values.
+
+    Returns
+    -------
+    DegreesOfFreedom
+        Primal degrees of freedom of the projection.
+    """
+    nodes, weights = _prepare_integration(
+        integration=integration, registry=integration_registry
+    )
+
+    func_vals = (
+        np.asarray(func(*[nodes[i, ...] for i in range(nodes.shape[0])])) * weights
+    )
+    del nodes, weights, func
+
+    int_space: IntegrationSpace
+    match integration:
+        case IntegrationSpace():
+            int_space = integration
+        case SpaceMap():
+            int_space = integration.integration_space
+        case _:
+            assert False
+
+    basis_values = function_space.values_at_integration_nodes(
+        int_space,
+        integration_registry=integration_registry,
+        basis_registry=basis_registry,
+    )
+    del integration_registry, basis_registry
+
+    func_vals = func_vals.flatten()
+    basis_values = basis_values.reshape((func_vals.size, -1))
+    dual_dofs = np.sum(func_vals[:, None] * basis_values, axis=0)
+    del func_vals, basis_values
+
+    mass_matrix = compute_mass_matrix(function_space, function_space, integration)
+    del integration
+
+    primal_dofs = np.linalg.solve(mass_matrix, dual_dofs)
+    del dual_dofs, mass_matrix
+    return DegreesOfFreedom(function_space, primal_dofs)
---
+++ tests/test_l2_proj.py
@@ -0,0 +1,128 @@
+"""Check that L2 projection works correctly."""
+
+from functools import partial
+
+import numpy as np
+import pytest
+from interplib._interp import (
+    BasisSpecs,
+    DegreesOfFreedom,
+    FunctionSpace,
+    IntegrationSpace,
+    IntegrationSpecs,
+)
+from interplib.degrees_of_freedom import reconstruct
+from interplib.domains import Quad
+from interplib.enum_type import BasisType
+from interplib.integration import projection_l2
+
+
+@pytest.mark.parametrize("order", (1, 2, 4, 10))
+@pytest.mark.parametrize("btype", BasisType)
+def test_projection_1d(order: int, btype: BasisType) -> None:
+    """Check that projection of DoFs to the same space is identity."""
+    fs = FunctionSpace(BasisSpecs(btype, order))
+
+    dofs = DegreesOfFreedom(fs)
+
+    rng = np.random.default_rng(129)
+    dofs.values = rng.random(dofs.shape)
+
+    int_space = IntegrationSpace(IntegrationSpecs(order + 2, method="gauss"))
+
+    proj = projection_l2(partial(reconstruct, dofs), fs, int_space)
+
+    assert proj.values == pytest.approx(dofs.values)
+
+
+@pytest.mark.parametrize(("o1", "o2"), ((1, 1), (2, 4), (10, 4)))
+@pytest.mark.parametrize(
+    ("b1", "b2"),
+    (
+        (BasisType.BERNSTEIN, BasisType.BERNSTEIN),
+        (BasisType.LEGENDRE, BasisType.LAGRANGE_CHEBYSHEV_GAUSS),
+    ),
+)
+def test_projection_2d(o1: int, b1: BasisType, o2: int, b2: BasisType) -> None:
+    """Check that projection of DoFs to the same space is identity."""
+    fs = FunctionSpace(BasisSpecs(b1, o1), BasisSpecs(b2, o2))
+
+    dofs = DegreesOfFreedom(fs)
+
+    rng = np.random.default_rng(129)
+    dofs.values = rng.random(dofs.shape)
+
+    int_space = IntegrationSpace(
+        IntegrationSpecs(o1 + 2, method="gauss"), IntegrationSpecs(o2 + 2, method="gauss")
+    )
+
+    proj = projection_l2(partial(reconstruct, dofs), fs, int_space)
+
+    assert proj.values == pytest.approx(dofs.values)
+
+
+@pytest.mark.parametrize(("o1", "o2", "o3"), ((1, 1, 1), (2, 4, 10), (10, 4, 3)))
+@pytest.mark.parametrize(
+    ("b1", "b2", "b3"),
+    (
+        (BasisType.BERNSTEIN, BasisType.BERNSTEIN, BasisType.LAGRNAGE_GAUSS),
+        (BasisType.LEGENDRE, BasisType.LAGRANGE_CHEBYSHEV_GAUSS, BasisType.BERNSTEIN),
+    ),
+)
+def test_projection_3d(
+    o1: int, b1: BasisType, o2: int, b2: BasisType, o3: int, b3: BasisType
+) -> None:
+    """Check that projection of DoFs to the same space is identity."""
+    fs = FunctionSpace(BasisSpecs(b1, o1), BasisSpecs(b2, o2), BasisSpecs(b3, o3))
+
+    dofs = DegreesOfFreedom(fs)
+
+    rng = np.random.default_rng(129)
+    dofs.values = rng.random(dofs.shape)
+
+    int_space = IntegrationSpace(
+        IntegrationSpecs(o1 + 2, method="gauss"),
+        IntegrationSpecs(o2 + 2, method="gauss"),
+        IntegrationSpecs(o3 + 2, method="gauss"),
+    )
+
+    proj = projection_l2(partial(reconstruct, dofs), fs, int_space)
+
+    assert proj.values == pytest.approx(dofs.values)
+
+
+def test_deformed_2d_to_3d() -> None:
+    """Check that projection of DoFs in deformed space works as expected."""
+
+    def test_function(*args):
+        x, y, z = args
+        return x**2 + y * z - 1
+
+    fs = FunctionSpace(
+        BasisSpecs("bernstein", 3),
+        BasisSpecs("bernstein", 3),
+    )
+
+    domain = Quad.from_corners(
+        (-2, -1, -1),
+        (+1.5, -1, +1),
+        (+1, +1, +1),
+        (-1, +1, -1),
+    )
+
+    int_space = IntegrationSpace(
+        IntegrationSpecs(5, method="gauss"),
+        IntegrationSpecs(5, method="gauss"),
+    )
+    smap = domain(int_space)
+
+    proj = projection_l2(test_function, fs, smap)
+
+    r1, r2 = np.meshgrid(np.linspace(-1, +1, 11), np.linspace(-1, +1, 11))
+    tx, ty, tz = domain.sample(r1, r2)
+
+    assert test_function(tx, ty, tz) == pytest.approx(reconstruct(proj, r1, r2))
+
+
+if __name__ == "__main__":
+    test_deformed_2d_to_3d()
